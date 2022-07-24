8634d4193251e4d324f46e408c6170363c6279b3
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-07-24 21:14:09 +0200

    Working demo

---
+++ ManagedApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnmanagedWrapper;
+
+namespace ManagedApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.Hello();
+        }
+    }
+}
